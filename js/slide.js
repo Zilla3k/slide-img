513faca410845cb9484aa03f3f0151a982de1cab
zilla3k <henriquepelanda.web@gmail.com>
2022-10-25 08:28:21 -0300

    slideNav-finalizada

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
 import debounce from "./debounce.js";
 
-export default class Slide {
+export class Slide {
   constructor(slide, wrap) {
     this.slide = document.querySelector(slide);
     this.wrap = document.querySelector(wrap);
@@ -139,6 +139,8 @@ export default class Slide {
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
     this.onResize = debounce(this.onResize.bind(this), 200);
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
   }
   init() {
     this.bindEvents();
@@ -146,6 +148,19 @@ export default class Slide {
     this.addSlideEvent();
     this.slideConfig();
     this.addResizeEvent();
+    this.changeSlide(0);
     return this;
+  }
+}
+export class SlideNav extends Slide {
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+
+  addArrowEvent() {
+    this.prevElement.addEventListener("click", this.activePrevSlide);
+    this.nextElement.addEventListener("click", this.activeNextSlide);
   }
 }
